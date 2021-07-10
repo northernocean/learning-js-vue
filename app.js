7a0215f00abf7aaf450775384c82d29f8864862f
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-10 19:54:33 -0400

    basic event binding

--- app.js
+++ app.js
@@ -2,7 +2,28 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      username: "",
     };
+  },
+  methods: {
+    incrementCounter() {
+      this.counter++;
+    },
+    decrementCounter() {
+      this.counter--;
+    },
+    inputChanged(e) {
+      console.log(e);
+      this.username = e.target.value;
+    },
+    inputUpdated(e) {
+      console.log(e);
+      this.username = '|' + e.target.value + '|';
+    },
+    submitForm(e) {
+      console.log(e);
+      alert("submitted");
+    },
   },
 });
 
