93d71b363f693b5bfb001d6ab1bb89064db949f1
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-11 20:41:57 -0400

    starting point - section 3

--- app.js
+++ app.js
@@ -1,23 +1,7 @@
 const app = Vue.createApp({
   data() {
-    return {
-      inputClass: '',
-      paragraphHidden: false,
-      paragraphBgColor: ''
-    }
+    return { goals: [] };
   },
-  computed: {
-    paraClasses() {
-      return {
-        user1: this.inputClass === 'user1',
-        user2: this.inputClass === 'user2',
-        hidden: this.paragraphHidden
-      };
-    }
-  },
-  methods: {
-    toggleParagraph() {
-      this.paragraphHidden = !this.paragraphHidden;
-    }
-  }
-}).mount('#assignment');
+});
+
+app.mount('#user-goals');
