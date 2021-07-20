f57cc635b3044b40a0163f9c603486b8b26cfb2e
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-20 15:33:17 -0400

    prepare project for assignment 6

--- src/main.js
+++ src/main.js
@@ -1,12 +1,16 @@
-import { createApp } from 'vue';
+import { createApp } from 'vue'
 
-import App from './App.vue';
-import FriendContact from './components/FriendContact.vue';
-import AddFriend from './components/AddFriend.vue';
+createApp({}).mount('#app')
 
-const app = createApp(App);
+// Task 1: 
+// Add two components to the app: 
+// An ActiveUser component and an UserData component
+// ActiveUser should output a username (h2) and age (h3)
+// UserData should output two input fields => for name and age
+// Optional: Add styling of your choice
 
-app.component('friend-contact', FriendContact);
-app.component('add-friend', AddFriend);
+// Task 2: Output both components side-by-side in your main App template
 
-app.mount('#app');
+// Task 3: Add user data and ensure it contains a name and age
+// User data should be output in ActiveUser
+// It should be updated via the UserData component
