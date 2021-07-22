7ff55b6b540aaaa67b3f9e45d7c1ffdc31e23ab5
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-22 14:04:33 -0400

    wip

---
+++ src/components/BaseCard.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="foo">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  export default {}
+</script>
+
+<style scoped>
+  div.foo {
+    margin: 2rem auto;
+    max-width: 30rem;
+    border-radius: 12px;
+    box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+    padding: 1rem;
+  }
+</style>
--- src/components/UserInfo.vue
+++ src/components/UserInfo.vue
@@ -1,29 +1,21 @@
 <template>
-  <section>
-    <div>
+  <base-card>
+    <div class="foo">
       <h3>{{ fullName }}</h3>
       <base-badge :type="role" :caption="role.toUpperCase()"></base-badge>
+      <p>{{ infoText }}</p>
     </div>
-    <p>{{ infoText }}</p>
-  </section>
+  </base-card>
 </template>
 
 <script>
 export default {
-  props: ['fullName', 'infoText', 'role'],
+  props: ["fullName", "infoText", "role"],
 };
 </script>
 
-<style>
-section {
-  margin: 2rem auto;
-  max-width: 30rem;
-  border-radius: 12px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  padding: 1rem;
-}
-
-section div {
+<style scoped>
+section header {
   display: flex;
   justify-content: space-between;
   align-items: center;
