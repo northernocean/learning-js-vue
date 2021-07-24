24d6ab59745a28f0487198de143d5472cb91c8a1
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-24 08:22:14 -0400

    add project files

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <ul>
+    <li v-for="resource in storedResources" :key="resource.id">
+      {{ resource.id }}
+    </li>
+  </ul>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        storedResources: [
+          { 
+            id: 'official-guide',
+            title: 'Official Guide',
+            description: 'The Official VueJS Documentation',
+            link: 'https://vuejs.org', 
+          },
+          { 
+            id: 'google',
+            title: 'Google',
+            description: 'learn to google...',
+            link: 'https://www.google.com', 
+          }
+        ]
+      };
+    },
+  }
+</script>
--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
 import { createApp } from 'vue';
 
-createApp({}).mount('#app');
+const app = createApp();
+
+app.mount('#app');
