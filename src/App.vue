522c6533ba19924f82588349e7b27c0247730b5c
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-26 10:44:35 -0400

    adding components

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,58 @@
 <template>
   <ul>
     <li v-for="resource in storedResources" :key="resource.id">
-      {{ resource.id }}
+      <resource-card
+        :title="resource.title"
+        :description="resource.id"
+      ></resource-card>
     </li>
   </ul>
+  <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
-  export default {
-    data() {
-      return {
-        storedResources: [
-          { 
-            id: 'official-guide',
-            title: 'Official Guide',
-            description: 'The Official VueJS Documentation',
-            link: 'https://vuejs.org', 
-          },
-          { 
-            id: 'google',
-            title: 'Google',
-            description: 'learn to google...',
-            link: 'https://www.google.com', 
-          }
-        ]
-      };
-    },
-  }
+import ResourceCard from './components/ResourceCard.vue';
+import StoredResources from './components/learning-resources/StoredResources.vue';
+
+export default {
+  components: {
+    'resource-card': ResourceCard,
+    'stored-resources': StoredResources,
+  },
+  data() {
+    return {
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official VueJS Documentation',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'learn to google...',
+          link: 'https://www.google.com',
+        },
+      ],
+    };
+  },
+};
 </script>
+
+<style scoped>
+li {
+  margin: 0.5rem;
+  list-style-type: none;
+}
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap');
+* {
+  box-sizing: border-box;
+}
+html {
+  font-family: 'Roboto', sans-serif;
+}
+body {
+  margin: 0;
+}
+</style>
---
+++ src/components/ResourceCard.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <h2>{{ title }} </h2>
+    <h4> {{ description }} </h4>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        Required: true
+      },
+      description: {
+        type: String,
+        Required: false,
+        default: ''
+      },
+    }
+  }
+
+</script>
+
+<style scoped>
+  div {
+    width: 50%;
+    margin-left: 10%;
+    border: 1px solid;
+    padding: .25rem;
+    border-radius: 20%;
+    border-color: rgba(110, 110, 110,.4);
+
+  }
+</style>
