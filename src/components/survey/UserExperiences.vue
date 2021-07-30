064de1b0adae76e384797a0d0f457840783bc7c5
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-07-30 07:55:50 -0400

    fetching data from firebase

--- src/components/survey/UserExperiences.vue
+++ src/components/survey/UserExperiences.vue
@@ -3,9 +3,12 @@
     <base-card>
       <h2>Submitted Experiences</h2>
       <div>
-        <base-button>Load Submitted Experiences</base-button>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
       </div>
-      <ul>
+      <p v-if="loadingSurveyData">Loading Survey Results...</p>
+      <ul v-else>
         <survey-result
           v-for="result in results"
           :key="result.id"
@@ -21,10 +24,33 @@
 import SurveyResult from './SurveyResult.vue';
 
 export default {
-  props: ['results'],
+  //props: ['results'],
   components: {
     SurveyResult,
   },
+  data() {
+    return {
+      results: [],
+      loadingSurveyData: false,
+    };
+  },
+  methods: {
+    loadExperiences() {
+      fetch(
+        'https://friendly-moss-feathers-default-rtdb.firebaseio.com/survey_data.json'
+      ).then(response => {
+        response.json().then((data) => {
+          this.loadingSurveyData = true;
+          this.results.length = 0;
+          Object.values(data).forEach((item) => this.results.push(item));
+          this.loadingSurveyData = false;
+        });
+      });
+    },
+  },
+  mounted() {
+    this.loadExperiences();
+  }
 };
 </script>
 
