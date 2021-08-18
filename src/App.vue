7e3c7f62580f616ca6a62fd06584424ee1cb2ffd
David Pecot <4504352+northernocean@users.noreply.github.com>
2021-08-18 16:34:48 -0400

    adding a second change counter button

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,25 @@
   <base-container title="Vuex">
     <the-counter></the-counter>
     <button @click="addOne">Add 1</button>
+    <change-counter></change-counter>
   </base-container>
 </template>
 
 <script>
 import BaseContainer from './components/BaseContainer.vue';
 import TheCounter from './components/TheCounter.vue';
+import ChangeCounter from './components/ChangeCounter.vue';
 
+// this is bad - we have two places updating state in two different ways...
 export default {
   components: {
     BaseContainer,
     TheCounter,
+    ChangeCounter,
   },
   methods: {
     addOne() {
-      this.$store.state.counter += 1;
+      this.$store.state.counter += 11;
     },
   },
 }
